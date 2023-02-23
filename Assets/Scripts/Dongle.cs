facf01de6d4838f2841924712b1672cc7aa1bec8
YoungKwang Nam <coolbirds@naver.com>
2023-02-24 05:16:31 +0900

    [20230223] first commit, Assets, etc

---
+++ Assets/Scripts/Dongle.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+    public int level;
+    public bool isDrag;
+    Rigidbody2D rigid;
+    Animator anim;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+    }
+
+    void Update()
+    {
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+
+
+            mousePos.y = 8;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CoinMerger
+
+
+Thanks to GoldMetal. (https://www.youtube.com/@goldmetal)  
+This game is a customized version of 2D mobile physics game of "DongleFamily" made by GoldMetal.(https://youtu.be/RvDaDt-469E).  
+
+I made my own dot sprite essets, the 'CoinFamily', and used them into this game, 'CoinMerger'.  
+
+[20230223] Preparing essets, Setting Andriod-building environment, Camera, background image, 2D-collider, and Respawning Animation.  
