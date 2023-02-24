94356b88bc52845e306073843b15ffa42d95934b
YoungKwang Nam <coolbirds@naver.com>
2023-02-24 14:15:55 +0900

    [20230224] Sound, GameOver, and ObjectPooling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,34 +4,102 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public Dongle lastDongle;
     public GameObject donglePrefab;
     public Transform dongleGroup;
+    public List<Dongle> donglePool;
+
+    public GameObject effectPrefab;
+    public Transform effectGroup;
+    public List<ParticleSystem> effectPool;
+
+    [Range(1, 30)]
+    public int poolSize;
+    public int poolCursor;
+    public Dongle lastDongle;
+
+    public AudioSource bgmPlayer;
+    public AudioSource[] sfxPlayer;
+    public AudioClip[] sfxClip;
+    public enum Sfx { LevelUp, Next, Attach, Button, Over} // 문자열로하면 간혹 오타로 인해 sfx를 못 불러오는 상황있으므로 방지 용도.
+    int sfxCursor;
+
+    public int score;
+    public int maxLevel;
+    public bool isOver;
 
     void Awake()
     {
         Application.targetFrameRate = 60;
+
+        donglePool = new List<Dongle>();
+        effectPool = new List<ParticleSystem>();
+        for(int index = 0; index < poolSize; index++)
+        {
+            MakeDongle();
+        }
     }
 
     void Start()
     {
+        bgmPlayer.Play();
         NextDongle();
+    }
+
+    Dongle MakeDongle()
+    {
+        // 이펙트 생성
+        GameObject instantEffectObj = Instantiate(effectPrefab, effectGroup);// effectGroup의 자식으로 생성됨.
+        instantEffectObj.name = "Effect " + effectPool.Count; 
+        ParticleSystem instantEffect = instantEffectObj.GetComponent<ParticleSystem>();
+        effectPool.Add(instantEffect);
+
+        // 동글 생성
+        GameObject instantDongleObj = Instantiate(donglePrefab, dongleGroup);// dongleGroup의 자식으로 생성됨.
+        instantDongleObj.name = "Dongle " + donglePool.Count;
+        Dongle instantDongle = instantDongleObj.GetComponent<Dongle>();
+        instantDongle.manager = this;
+        instantDongle.effect = instantEffect;
+        donglePool.Add(instantDongle);
+
+        return instantDongle;
     }
 
     Dongle GetDongle()
     {
-        GameObject instant = Instantiate(donglePrefab, dongleGroup);// dongleGroup의 자식으로 생성됨.
-        Dongle instantDongle = instant.GetComponent<Dongle>();
-        return instantDongle;
+        for (int index = 0; index < donglePool.Count; index++)
+        {
+            poolCursor = (poolCursor + 1) % donglePool.Count;
+            if (!donglePool[poolCursor].gameObject.activeSelf)
+            {
+                return donglePool[poolCursor];
+            }
+        }
+
+
+        //// 이펙트 생성
+        //GameObject instantEffectObj = Instantiate(effectPrefab, effectGroup);// effectGroup의 자식으로 생성됨.
+        //ParticleSystem instantEffect = instantEffectObj.GetComponent<ParticleSystem>();
+
+        //// 동글 생성
+        //GameObject instantDongleObj = Instantiate(donglePrefab, dongleGroup);// dongleGroup의 자식으로 생성됨.
+        //Dongle instantDongle = instantDongleObj.GetComponent<Dongle>();
+        //instantDongle.effect = instantEffect;
+        //return instantDongle;
+        return MakeDongle();
     }
 
     void NextDongle()
     {
-        Dongle newDongle = GetDongle();
-        lastDongle = newDongle;
-        lastDongle.level = Random.Range(0, 7);
-        lastDongle.gameObject.SetActive(true);
+        if (isOver)
+        {
+            return;
+        }
 
+        lastDongle = GetDongle();
+        lastDongle.level = Random.Range(0, maxLevel);
+        lastDongle.gameObject.SetActive(true);
+        
+        SfxPlay(Sfx.Next);
         StartCoroutine("WaitNext");
     }
 
@@ -42,7 +110,7 @@ public class GameManager : MonoBehaviour
             yield return null;
         }
 
-        yield return new WaitForSeconds(2.5f);
+        yield return new WaitForSeconds(2.0f);
 
         NextDongle();
     }
@@ -62,5 +130,65 @@ public class GameManager : MonoBehaviour
 
         lastDongle.Drop();
         lastDongle = null;
+    }
+
+    public void GameOver()
+    {
+        if (isOver)
+        {
+            return;
+        }
+
+        isOver = true;
+
+        StartCoroutine("GameOverRoutine");
+    }
+
+    IEnumerator GameOverRoutine()
+    {
+        // 1. 장면 안에 활성화 되어있는 모든 동글 가져오기.
+        Dongle[] dongles = FindObjectsOfType<Dongle>();
+
+        // 2. 지우기 전에 모든 동글의 물리효과 비활성화.
+        for (int index = 0; index < dongles.Length; index++)
+        {
+            dongles[index].rigid.simulated = false;
+        }
+
+
+        // 3. 1번의 목록을 하나씩 접근해서 지우기.
+        for (int index = 0; index < dongles.Length; index++)
+        {
+            dongles[index].Hide(Vector3.up * 100);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        yield return new WaitForSeconds(1f);
+        SfxPlay(Sfx.Over);
+    }
+
+    public void SfxPlay(Sfx type)
+    {
+        switch (type)
+        {
+            case Sfx.LevelUp:
+                sfxPlayer[sfxCursor].clip = sfxClip[Random.Range(0, 3)];
+                break;
+            case Sfx.Next:
+                sfxPlayer[sfxCursor].clip = sfxClip[3];
+                break;
+            case Sfx.Attach:
+                sfxPlayer[sfxCursor].clip = sfxClip[4];
+                break;
+            case Sfx.Button:
+                sfxPlayer[sfxCursor].clip = sfxClip[5];
+                break;
+            case Sfx.Over:
+                sfxPlayer[sfxCursor].clip = sfxClip[6];
+                break;
+        }
+
+        sfxPlayer[sfxCursor].Play();
+        sfxCursor = (sfxCursor + 1) % sfxPlayer.Length; // 0, 1, 2, 0, 1, 2...
     }
 }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ This game is a customized version of 2D mobile physics game of "DongleFamily" ma
 I made my own dot sprite essets, the 'CoinFamily', and used them into this game, 'CoinMerger'.  
 
 [20230223] Preparing essets, Setting Andriod-building environment, Camera, background image, 2D-collider, and Respawning Animation.  
+[20230224] Sound(BGM, SFX), GameOver condition, and ObjectPooling.  
